9f2557166fdc4a4bc10ba43af54bb9cd6ca787ca
Ayush Sahu <115640976+ayushS22@users.noreply.github.com>
2025-06-14 21:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 //complete this code
 class Person {
- 	constructor(name , age){
-		this.name = name ;
-		this.age = age;
-	}
-	get Name(){
-		return this.name;
-	}
-	set Age(Newage){
-		this.age =30;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-  	study(){
-		console.log(`${student.name} is studying`);
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-
-	teach(){
-		console.log(`${teacher.name} is studying`);
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
 
 // Do not change the code below this line
 window.Person = Person;
